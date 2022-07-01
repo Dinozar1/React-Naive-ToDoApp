77639ede8d7b0e117a3edba022d3706444cde7d3
Szymon Tomczak <80458007+Dinozar1@users.noreply.github.com>
2022-07-01 12:36:21 +0200

    adding files

--- App.js
+++ App.js
@@ -1,20 +1,122 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, TextInput, Button, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function App() {
+  const [list, setList] = useState([])
+  const [input,setInput] = useState('')
+
+  const addNewTask= () => 
+  {
+    if(input.length > 3)
+    {
+      if(list.length > 0)
+      {
+        const lastKey = list[list.length-1].key
+        setList(list => [...list, {text: input, key: lastKey+1}])
+      }
+      else
+      {
+        setList(list => [...list, {text: input, key: 1}])
+      }
+    }
+    else
+    {
+        Alert.alert('Oops!', 'ToDos must be over 3 chars long', [
+          {text: 'Understood'}
+        ])
+    }
+    
+    
+  }
+
+  const deleteItem = (k) => 
+  {
+    const newArr = list.filter((element) => element.key !== k)
+    setList(newArr)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Text style={styles.header}>To do List</Text>
+        <View style={styles.inputDiv}>
+          <Text>Insert Task:</Text>
+          <TextInput 
+            style={styles.input}
+            onChangeText={(value) => setInput(value)}
+            value={input}
+          />
+          <Button 
+            onPress={() => addNewTask()}
+            style={styles.button}
+            title="Add task"
+            color="#841584"
+          />
+        </View>
+        <View style={styles.listDiv}>
+          <FlatList
+            data={list}
+            renderItem={({item}) => 
+            <TouchableOpacity onPress={() => deleteItem(item.key)}>
+              <View style={styles.element}>
+                <View style={styles.icon}>
+                  <MaterialIcons name="delete" size={24} color="#333" />
+                  <Text style={styles.itemText}>{item.text}</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+            }
+          />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:
+  {
+    backgroundColor: 'pink',
+    paddingVertical: 30,
+    paddingHorizontal: 20,
+    flex: 1
   },
+  header:
+  {
+    fontSize: 40,
+    textAlign: 'center'
+  },
+  input:
+  {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 5,
+  },
+  button:
+  {
+    marginTop: 20,
+  },
+  element:
+  {
+    padding: 10,
+    borderWidth: 1,
+    borderStyle: 'dashed',
+    borderColor: 'grey',
+    margin: 10,
+    width: 300,
+    borderRadius: 5
+  },
+  listDiv:
+  {
+    flex:1
+  },
+  icon:
+  {
+    flexDirection: 'row',
+  },
+  itemText:
+  {
+    paddingHorizontal: 10,
+  }
 });
